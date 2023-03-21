1d3b3edf6ed7318484c7c72746bceb3806613253
GuilhermeFernandesCC <74461094+GuilhermeFernandesCC@users.noreply.github.com>
2023-03-21 18:41:00 -0300

    Cliente, Fachada

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,7 +1,16 @@
+import java.sql.Date;
+import java.util.Arrays;
+
 public class Cliente {
     private static Fachada fachada = new Fachada();
     public static void main(String[] args) {
-        fachada.listarProdutos();
+        System.out.println(Arrays.toString(fachada.listarProdutos()));
 
+        fachada.addProduto("Leite","Mimosa",6.50);
+
+        System.out.println(Arrays.toString(fachada.listarProdutos()));
+
+        Date validade = new Date(2023,9,23);
+        fachada.addLote(fachada.listarProdutos()[0],25,validade);
     }
 }
--- src/Fachada.java
+++ src/Fachada.java
@@ -1,12 +1,18 @@
+import java.sql.Date;
+
 public class Fachada {
     private LoteServices loteServices = new LoteServices();
     private ProdutoServices produtoServices = new ProdutoServices();
 
-    public String adaProduto(String nome,String fabricante,double preco){
+    public String addProduto(String nome,String fabricante,double preco){
         return produtoServices.addProduto(nome,fabricante,preco);
     }
 
     public String[] listarProdutos(){
         return produtoServices.listarProdutos();
+    }
+
+    public String addLote(String prodID, int quant, Date validade ){
+        return loteServices.addLote(prodID,quant,validade);
     }
 }
